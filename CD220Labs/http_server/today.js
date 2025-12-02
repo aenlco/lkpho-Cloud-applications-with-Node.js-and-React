b8a49e8c5abebf782e41012a440c31a1466475ad
aenlco <renee@aenl.co>
2025-12-02 18:50:27 -0500

    Made changes to today.js file

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,7 +1,7 @@
  // Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
     // Get the current date and time string in the timezone "Australia/Brisbane"
-    let aestString = new Date().toLocaleString("en-US", { timeZone: "Australia/Brisbane" });
+    let aestString = new Date().toLocaleString("en-US", { timeZone: "Chicago" });
     // Convert that string back into a Date object
     let aestDate = new Date(aestString);
     return aestDate; 
